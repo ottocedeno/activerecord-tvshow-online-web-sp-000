dcc7d47022049632df4c1d814d95f16ec490161f
Otto Cedeno <otto@ottostacos.com>
2020-02-14 03:31:46 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.where("rating = ?", highest_rating).first
+    self.where("rating = ?", self.highest_rating).first
   end
 
   def self.lowest_rating
